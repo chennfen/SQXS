358b7cda6da684bd6cb8fe28e306e89f90108fca
chennfen <34900481+chennfen@users.noreply.github.com>
2021-03-16 22:03:10 +0800

    Update sqxs10_read.js

--- Task/sqxs10_read.js
+++ Task/sqxs10_read.js
@@ -109,17 +109,17 @@ async function all() {
 
                 
                 //看视频奖励抽奖次数
-                await vediodrawprize(0);
+                //await vediodrawprize(0);
                 
                 //看视频奖励金币
-                await vediogoldprize(0);
+                //await vediogoldprize(0);
                 
                 //阅读
-                //await readbook();
+                await readbook();
 
                 //收集阅读金币
                 //if(ReadTimes>0)
-                //await receivecoin();
+                await receivecoin();
 
                 //个人信息
                 await userinfo();
