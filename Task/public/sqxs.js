e359f92904d1031cb775825457d7f0267e24e442
chennfen <34900481+chennfen@users.noreply.github.com>
2021-03-21 22:58:58 +0800

    Update sqxs.js

--- Task/public/sqxs.js
+++ Task/public/sqxs.js
@@ -131,14 +131,14 @@ async function all() {
                 vediogold = 0;
                 drawgold = 0;
                 //阅读
-                //await readbook();
+                await readbook();
 
                 //收集阅读金币
                 //if(ReadTimes>0)
-                //await receivecoin();
+                await receivecoin();
 
                 //看视频奖励金币
-                //await vediogoldprize(0);
+                await vediogoldprize(0);
 
                 //看视频奖励抽奖次数
                 await vediodrawprize(0);
