edd99d1c080be0289473bc85694ffb649d50ef50
zsl119 <34900481+zsl119@users.noreply.github.com>
2021-04-05 16:49:10 +0800

    Update sqxsgetck.js

--- Task/test/sqxsgetck.js
+++ Task/test/sqxsgetck.js
@@ -21,7 +21,7 @@ if（url.indexOf（'/ ad / v1 / api / prize / readpage / pendant / lottery'）�
 
 
 
-if（url.indexOf（'/ jbookmark / v1 / api / prize / readmark / pendant / lottery'）！=-1）
+if（url.indexOf（'/ api / jbookmark / v1 / readmark / sync'）！=-1）
 {
 	var body = $ request.body;
 	var headers = $ request.headers;
