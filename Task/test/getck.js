9cca909bcd4ed6cc569a1bcf62ae4ebced261757
zsl119 <34900481+zsl119@users.noreply.github.com>
2021-04-07 09:09:07 +0800

    Update getck.js

--- Task/test/getck.js
+++ Task/test/getck.js
@@ -23,17 +23,19 @@ if(url.indexOf('/ad/v1/api/prize/readpage/pendant/lottery')!=-1)
 //极速版阅读
 if(url.indexOf('/api/activity/v1/activity/pendant/lottery')!=-1)
 {
+	
+	
 	var body = $request.body;
 	var headers = $request.headers;
 	if(body&&headers)
 	{
+
 		$.msg('书旗小说极速版'+ $.idx,'获取阅读ck成功(共2个ck)');
 
 		let read2ck=body+'&&'+JSON.stringify(headers);
 		$.setdata(read2ck ,'read2ck'+ $.idx)	
 	}		
 }
-
 
 
 if(url.indexOf('/prizecenter/xapi/prize/manual/receive')!=-1)
@@ -86,7 +88,7 @@ if(url.indexOf('/api/ad/v1/api/prize/lottery')!=-1)
 		$.setdata(vediogold2prizeck ,'vediogold2prizeck'+ $.idx)	
 	}	
 
-
+}
 
 if(url.indexOf('activity/activity/v1/lottery/draw')!=-1)
 {
